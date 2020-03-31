978cf1cf5ce27193cef2905fb6f961ee7031a70e
bstonesh <brandon@stone.sh>
2020-03-31 12:25:34 -0700

    optimized code using Python 'with' keyword, easier to read; specifying offset in print statement before patching file

--- patch_goga.py
+++ patch_goga.py
@@ -24,26 +24,23 @@ def gen_key_patch(file_patch, file_offset, private_key_file='private_key.pem', p
     print('Backing up file (%s)...' % file_patch)
     copyfile(file_patch, file_patch + '_backup')
 
-    print('Patching provided file (%s) using offset...' % file_patch)  # TODO: specify offset
-    patch_file = open(file_patch, 'r+b')  # open file to patch
-    patch_file.seek(file_offset)  # move to public key
-    parsed_public_key = pub_exported[27:-25]  # parse public key data
-    patch_file.write(parsed_public_key)  # parses extra from PEM public key
-    patch_file.close()  # close file
+    print('Patching provided file (%s) using offset (%s)...' % (file_patch, hex(file_offset)))
+    with open(file_patch, 'r+b') as patch_file:  # open file to patch
+        patch_file.seek(file_offset)  # move to public key
+        parsed_public_key = pub_exported[27:-25]  # parse public key data
+        patch_file.write(parsed_public_key)  # parses extra from PEM public key
     print('File (%s) patched!' % file_patch)
 
     # PRIVATE KEY FILE
     print('Writing new private key to %s...' % private_key_file)
-    priv_file = open(private_key_file, 'wb')  # opens new private key file
-    priv_file.write(prv_exported)  # write private key data to file
-    priv_file.close()  # close private file
+    with open(private_key_file, 'wb') as priv_file: # opens new private key file
+        priv_file.write(prv_exported)  # write private key data to file
     print('New private key written to %s!' % private_key_file)
 
     # PUBLIC KEY FILE
     print('Writing new public key to %s...' % public_key_file)
-    pub_file = open(public_key_file, 'wb')  # opens new public key file
-    pub_file.write(pub_exported)  # write public key data to file
-    pub_file.close()  # close public file
+    with open(public_key_file, 'wb') as pub_file:  # opens new public key file
+        pub_file.write(pub_exported)  # write public key data to file
     print('New public key written to %s!' % public_key_file)
 
 
